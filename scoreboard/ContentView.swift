fd7d6bcf55c6a594d17b3000c519288a624672d3
sunche243 <starcj7@naver.com>
2023-10-29 23:12:34 +0900

    Initial Commit

---
+++ scoreboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scoreboard
+//
+//  Created by 박찬준 on 10/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scoreboard/scoreboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoreboardApp.swift
+//  scoreboard
+//
+//  Created by 박찬준 on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct scoreboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
